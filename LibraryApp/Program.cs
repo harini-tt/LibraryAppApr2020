cac3b4cc8e5dc19c4d618ee6aed59c896da32e67
harini-tt <harinitt9@gmail.com>
2020-04-17 21:00:43 -0700

    Initial check-in of module LibraryApp

---
+++ LibraryApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myFirstAccount = new Account();
+            myFirstAccount.AccountNumber = 990;
+            myFirstAccount.UserName = "harini";
+            myFirstAccount.EmailAddress = "harini@test.com";
+            myFirstAccount.PhoneNumber = 1234567890;
+            myFirstAccount.Return(1);
+            myFirstAccount.Checkout(2);
+
+            var mySecondAccount = new Account();
+            mySecondAccount.AccountNumber = 123;
+            mySecondAccount.UserName = "harinitt";
+            mySecondAccount.EmailAddress = "harinitt@test.com";
+            mySecondAccount.PhoneNumber = 2345678901;
+            mySecondAccount.Return(2);
+            mySecondAccount.Checkout(1);
+
+        }
+    }
+}
