50367cdba2bd0e5de1fa6a5afe1760211ba6be71
harini-tt <harinitt9@gmail.com>
2020-04-18 10:55:58 -0700

    Made changes to how user info is stored.

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -6,21 +6,18 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
-            var myFirstAccount = new Account();
-            myFirstAccount.AccountNumber = 990;
-            myFirstAccount.UserName = "harini";
-            myFirstAccount.EmailAddress = "harini@test.com";
-            myFirstAccount.PhoneNumber = 1234567890;
-            myFirstAccount.Return(1);
-            myFirstAccount.Checkout(2);
+            var myFirstAccount = LibraryAcc.CreateAccount("Bob Smith", "smithb", TypesofAccounts.Teen, 9999, "bob.smith@test.com", 1234567890);
+            myFirstAccount.CheckoutBook(2);
+            // myFirstAccount.CheckoutMovie(1);
+            // myFirstAccount.CheckoutMagazine(6);
+            Console.WriteLine($"AN: {myFirstAccount.AccountNumber}, Name: {myFirstAccount.UserName}, Account Type: {myFirstAccount.AccountType}, Email: {myFirstAccount.EmailAddress}, Phone: {myFirstAccount.PhoneNumber}, Created Date: {myFirstAccount.CreatedDate}, Books Balance: {myFirstAccount.BooksBalance}");
 
-            var mySecondAccount = new Account();
-            mySecondAccount.AccountNumber = 123;
-            mySecondAccount.UserName = "harinitt";
-            mySecondAccount.EmailAddress = "harinitt@test.com";
-            mySecondAccount.PhoneNumber = 2345678901;
-            mySecondAccount.Return(2);
-            mySecondAccount.Checkout(1);
+            var mySecondAccount = LibraryAcc.CreateAccount("Melissa Jones","jonesm", TypesofAccounts.Adult, 8181, "melissa.jones@test.com", 0987654321);
+            mySecondAccount.ReturnBook(2);
+            mySecondAccount.CheckoutBook(10);
+            // mySecondAccount.CheckoutMovie(1);
+            // mySecondAccount.CheckoutMagazine(6);
+            Console.WriteLine($"AN: {mySecondAccount.AccountNumber}, Name: {mySecondAccount.UserName}, Account Type: {mySecondAccount.AccountType}, Email: {mySecondAccount.EmailAddress}, Phone: {mySecondAccount.PhoneNumber}, Created Date: {mySecondAccount.CreatedDate}, Books Balance: {mySecondAccount.BooksBalance}");
 
         }
     }
